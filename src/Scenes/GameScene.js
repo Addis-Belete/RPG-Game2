13a13251bff57363baeec252f8df44ece9102366
Addis Belete <addis.bel@gmail.com>
2021-07-05 08:00:45 +0300

    Add player to world

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -8,6 +8,6 @@ export default class GameScene extends Phaser.Scene {
 
 
 	create() {
-		this.scene.start('WorldScene');
+		this.scene.start('World');
 	}
 };
--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -1,21 +1,26 @@
-const WorldScene = new Phaser.Class({
 
-	Extends: Phaser.Scene,
+import 'phaser';
 
-	initialize:
+export default class GameScene extends Phaser.Scene {
+	constructor() {
+		super('World');
+	}
 
-		function WorldScene() {
-			Phaser.Scene.call(this, { key: 'WorldScene' });
-		},
-	preload: function () {
+	preload() {
 
-	},
-	create: function () {
+	}
+
+	create() {
 		const map = this.make.tilemap({ key: 'map' });
 		const tiles = map.addTilesetImage('spritesheet', 'tiles');
 
 		const grass = map.createStaticLayer('Grass', tiles, 0, 0);
 		const obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
 		obstacles.setCollisionByExclusion([-1]);
+		this.player = this.physics.add.sprite(50, 100, 'player', 6);
+		this.physics.world.bounds.width = map.widthInPixels;
+		this.physics.world.bounds.height = map.heightInPixels;
+		this.player.setCollideWorldBounds(true);
+
 	}
-});
+};
