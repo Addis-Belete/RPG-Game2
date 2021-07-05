35d225cda7b2109e8fc55ff19bbc3522d4d14635
Addis Belete <addis.bel@gmail.com>
2021-07-05 08:10:35 +0300

    Add animation to player

--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -1,6 +1,4 @@
-
 import 'phaser';
-
 export default class GameScene extends Phaser.Scene {
 	constructor() {
 		super('World');
@@ -25,25 +23,50 @@ export default class GameScene extends Phaser.Scene {
 		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 		this.cameras.main.startFollow(this.player);
 		this.cameras.main.roundPixels = true;
+		//  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+		this.anims.create({
+			key: 'left',
+			frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+			frameRate: 10,
+			repeat: -1
+		});
+
+		// animation with key 'right'
+		this.anims.create({
+			key: 'right',
+			frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+			frameRate: 10,
+			repeat: -1
+		});
+		this.anims.create({
+			key: 'up',
+			frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14] }),
+			frameRate: 10,
+			repeat: -1
+		});
+		this.anims.create({
+			key: 'down',
+			frames: this.anims.generateFrameNumbers('player', { frames: [0, 6, 0, 12] }),
+			frameRate: 10,
+			repeat: -1
+		});
 
 	}
 	update(time, delta) {
-		this.player.body.setVelocity(0);
-
-		// Horizontal movement
 		if (this.cursors.left.isDown) {
-			this.player.body.setVelocityX(-80);
+			this.player.anims.play('left', true);
 		}
 		else if (this.cursors.right.isDown) {
-			this.player.body.setVelocityX(80);
+			this.player.anims.play('right', true);
 		}
-
-		// Vertical movement
-		if (this.cursors.up.isDown) {
-			this.player.body.setVelocityY(-80);
+		else if (this.cursors.up.isDown) {
+			this.player.anims.play('up', true);
 		}
 		else if (this.cursors.down.isDown) {
-			this.player.body.setVelocityY(80);
+			this.player.anims.play('down', true);
+		}
+		else {
+			this.player.anims.stop();
 		}
 	}
 };
