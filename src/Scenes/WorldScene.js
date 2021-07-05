1d3237a3c40173832499168cb554c5a6347a4ad8
Addis Belete <addis.bel@gmail.com>
2021-07-05 08:05:04 +0300

    Make Player move

--- src/Scenes/WorldScene.js
+++ src/Scenes/WorldScene.js
@@ -21,6 +21,29 @@ export default class GameScene extends Phaser.Scene {
 		this.physics.world.bounds.width = map.widthInPixels;
 		this.physics.world.bounds.height = map.heightInPixels;
 		this.player.setCollideWorldBounds(true);
+		this.cursors = this.input.keyboard.createCursorKeys();
+		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+		this.cameras.main.startFollow(this.player);
+		this.cameras.main.roundPixels = true;
 
+	}
+	update(time, delta) {
+		this.player.body.setVelocity(0);
+
+		// Horizontal movement
+		if (this.cursors.left.isDown) {
+			this.player.body.setVelocityX(-80);
+		}
+		else if (this.cursors.right.isDown) {
+			this.player.body.setVelocityX(80);
+		}
+
+		// Vertical movement
+		if (this.cursors.up.isDown) {
+			this.player.body.setVelocityY(-80);
+		}
+		else if (this.cursors.down.isDown) {
+			this.player.body.setVelocityY(80);
+		}
 	}
 };
