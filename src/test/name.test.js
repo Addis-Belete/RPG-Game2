2bfa81bd89247a0ba0b6e66edf0237fa9456f5de
Addis Belete <addis.bel@gmail.com>
2021-07-07 08:45:56 +0300

    Install babel

---
+++ src/test/name.test.js
@@ -0,0 +1,18 @@
+import { setPlayerName, getPlayerName } from '../Score/Name';
+test("Default player name should be 'Guest'", () => {
+	expect(getPlayerName()).toBe('Guest');
+});
+
+test("If name input field is empty name should be 'Guest'", () => {
+	setPlayerName('');
+	expect(getPlayerName()).toBe('Guest');
+});
+
+test('Player name must be a string', () => {
+	expect(typeof getPlayerName()).toBe('string');
+});
+
+test('Successfully set player name', () => {
+	setPlayerName('Addis');
+	expect(getPlayerName()).toBe('Addis');
+});
