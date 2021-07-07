112b5ca20a55b6fe623d2fc03962492985113715
Addis Belete <addis.bel@gmail.com>
2021-07-07 12:02:42 +0300

    Fix eslint error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
-	res.sendFile(HTML_FILE);
+  res.sendFile(HTML_FILE);
 });
 
 const PORT = process.env.PORT || 8080;
