3d4ad44e284125790656989e8d1ad8f75b2b9d5b
Addis Belete <addis.bel@gmail.com>
2021-07-07 19:46:07 +0300

    Move Assets to dis folder

--- webpack/base.js
+++ webpack/base.js
@@ -4,41 +4,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
-        },
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: 'file-loader',
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, '../'),
-    }),
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						plugins: ['@babel/plugin-transform-runtime'],
+					},
+				},
+			},
+			{
+				test: [/\.vert$/, /\.frag$/],
+				use: 'raw-loader',
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg|xml)$/i,
+				use: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+			root: path.resolve(__dirname, '../'),
+		}),
+		new webpack.DefinePlugin({
+			CANVAS_RENDERER: JSON.stringify(true),
+			WEBGL_RENDERER: JSON.stringify(true),
+		}),
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+	],
 };
