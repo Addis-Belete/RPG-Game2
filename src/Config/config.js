e86d279504814571665cc12b799b8e081a186fb7
Addis Belete <addis.bel@gmail.com>
2021-07-07 22:04:10 +0300

    Remove rectangular cover

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,23 +1,23 @@
 import Phaser from 'phaser';
 
 export default {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  pixelArt: true,
-  dom: {
-    createContainer: true,
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: true,
-    },
-  },
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+	type: Phaser.AUTO,
+	parent: 'phaser-example',
+	width: 800,
+	height: 600,
+	pixelArt: true,
+	dom: {
+		createContainer: true,
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 0 },
+
+		},
+	},
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+	},
 };
--- src/Score/Api.js
+++ src/Score/Api.js
@@ -1,58 +1,58 @@
 const fetch = require('node-fetch');
 
 const sortData = (object) => {
-  const arr = [];
-  for (let i = 0; i < object.length; i += 1) {
-    arr.push([object[i].user, object[i].score]);
-  }
-  const sortedArr = Array.from(arr).sort((a, b) => b[1] - a[1]);
-  return sortedArr;
+	const arr = [];
+	for (let i = 0; i < object.length; i += 1) {
+		arr.push([object[i].user, object[i].score]);
+	}
+	const sortedArr = Array.from(arr).sort((a, b) => b[1] - a[1]);
+	return sortedArr;
 };
 
 const submitHighScore = async (
-  gameId = 'owXocNZgZCRk7sE9EfYl',
-  name,
-  score,
+	gameId = 'sE9vcMfTu6omIF4NoCp3',
+	name,
+	score,
 ) => {
-  const submit = {
-    user: name,
-    score,
-  };
-  const post = JSON.stringify(submit);
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: post,
-    });
-    const answer = await response.json();
-    return answer;
-  } catch (error) {
-    return error;
-  }
+	const submit = {
+		user: name,
+		score,
+	};
+	const post = JSON.stringify(submit);
+	const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+	try {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: post,
+		});
+		const answer = await response.json();
+		return answer;
+	} catch (error) {
+		return error;
+	}
 };
 
-const getHighScores = async (gameId = 'owXocNZgZCRk7sE9EfYl') => {
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
+const getHighScores = async (gameId = 'sE9vcMfTu6omIF4NoCp3') => {
+	const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+	try {
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
 
-    const answer = await response.json();
-    const sortedData = sortData(answer.result);
-    return sortedData;
-  } catch (error) {
-    return error;
-  }
+		const answer = await response.json();
+		const sortedData = sortData(answer.result);
+		return sortedData;
+	} catch (error) {
+		return error;
+	}
 };
 
 export { submitHighScore, getHighScores };
